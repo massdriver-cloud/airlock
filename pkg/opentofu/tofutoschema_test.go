f4b001fff4c92708520e8180a3f4836febd32fdb
chrisghill <chris@massdriver.cloud>
2025-07-11 15:46:15 -0600

    Emit useful errors when encountering type 'any' in HCL

--- pkg/opentofu/tofutoschema_test.go
+++ pkg/opentofu/tofutoschema_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/massdriver-cloud/airlock/pkg/opentofu"
@@ -14,29 +15,47 @@ import (
 func TestTofuToSchema(t *testing.T) {
 	type testData struct {
 		name string
+		err  string
 	}
 	tests := []testData{
 		{
 			name: "simple",
+		},
+		{
+			name: "any",
+			err:  "type 'any' cannot be converted to a JSON schema type",
+		},
+		{
+			name: "nestedany",
+			err:  "dynamic types are not supported (are you using type 'any'?)",
 		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			modulePath := filepath.Join("testdata/opentofu", tc.name)
 
-			want, err := os.ReadFile(filepath.Join("testdata/opentofu", tc.name, "schema.json"))
-			if err != nil {
-				t.Fatalf("%d, unexpected error", err)
-			}
-
 			got, err := opentofu.TofuToSchema(modulePath)
-			if err != nil {
-				t.Fatalf("%d, unexpected error", err)
+			if err != nil && tc.err == "" {
+				t.Fatalf("unexpected error: %s", err.Error())
+			}
+			if tc.err != "" && err == nil {
+				t.Fatalf("expected error %s, got nil", tc.err)
+			}
+			if tc.err != "" && !strings.Contains(err.Error(), tc.err) {
+				t.Fatalf("expected error %s, got %s", tc.err, err.Error())
+			}
+			if tc.err != "" {
+				return
 			}
 
 			bytes, err := json.Marshal(got)
 			if err != nil {
-				t.Fatalf("%d, unexpected error", err)
+				t.Fatalf("unexpected error: %s", err.Error())
+			}
+
+			want, err := os.ReadFile(filepath.Join("testdata/opentofu", tc.name, "schema.json"))
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err.Error())
 			}
 
 			require.JSONEq(t, string(want), string(bytes))
