7a981bce9eab3c262da30225246fb57247abf657
chrisghill <chris@massdriver.cloud>
2025-08-19 15:46:26 -0600

    lint

--- pkg/result/print.go
+++ pkg/result/print.go
@@ -7,12 +7,12 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-func (r *SchemaResult) PrettyDiags() string {
+func (result *SchemaResult) PrettyDiags() string {
 	warningString := lipgloss.NewStyle().SetString("WARNING").Foreground(lipgloss.Color("#FFA500"))
 	errorString := lipgloss.NewStyle().SetString("ERROR").Foreground(lipgloss.Color("#FF0000"))
 
 	output := ""
-	for _, diag := range r.Diags {
+	for _, diag := range result.Diags {
 		levelString := warningString
 		if diag.Level == Error {
 			levelString = errorString
